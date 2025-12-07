4430a15f6c18cea226ed0509a5ad2e6118018f3d
KnOkEr350 <rootimt590@gmail.com>
2025-12-07 19:18:19 +0300

    Создал dockerfile для FastAPI

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello from VM!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id, "name": f"Item {item_id}"}
+
